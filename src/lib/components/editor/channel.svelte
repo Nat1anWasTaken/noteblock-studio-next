02ebca1fe6de6d72ef481c51381a2dd9adc7101a
Nat1anWasTaken <nathan@nat1an.xyz>
2025-09-14 13:29:40 +0800

    feat(editor.svelte): make the channel control resizable

---
+++ src/lib/components/editor/channel.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { Channel } from '$lib/types';
+
+    interface Props {
+        channel: Channel;
+    }
+
+    let { channel }: Props = $props();
+</script>
--- src/lib/components/editor/editor.svelte
+++ src/lib/components/editor/editor.svelte
@@ -1,4 +1,5 @@
 <script>
+    import * as Resizable from '$lib/components/ui/resizable';
     import { player } from '$lib/playback.svelte';
     import { sample } from '$lib/sample-song';
     import { onMount } from 'svelte';
@@ -12,9 +13,15 @@
 <div class="flex h-screen flex-col">
     <EditorHeader />
 
-    <div class="flex-1 overflow-scroll">
-        {#each { length: 200 } as a}
-            <p>a</p>
-        {/each}
-    </div>
+    <Resizable.PaneGroup direction="horizontal">
+        <Resizable.Pane
+            class="flex flex-col items-center justify-center bg-accent"
+            defaultSize={32}
+        >
+            <h1>Channel Control</h1>
+            <p class="text-muted-foreground">Placeholder for channel control...</p>
+        </Resizable.Pane>
+        <Resizable.Handle />
+        <Resizable.Pane>Channels</Resizable.Pane>
+    </Resizable.PaneGroup>
 </div>
